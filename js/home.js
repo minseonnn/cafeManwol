81dcd344481163d2df5eb2a657d19b4dc9a4cfaa
minseonnn <119590961+minseonnn@users.noreply.github.com>
2023-01-21 06:41:36 +0900

    js update

---
+++ js/home.js
@@ -0,0 +1,18 @@
+//스크롤 이벤트 
+let mhtml = ${"html"};
+let page = 1;
+
+mHtml.animate({scrollTop : 0},10);
+
+$(window).on("wheel", function(e) {
+  if(mHtml.is(":animated")) return;
+  if(e.originalEvent.deltaY > 0) {
+      if(page == 4) return;
+      page++;
+  } else if(e.originalEvent.deltaY < 0) {
+      if(page == 1) return;
+      page--;
+  }
+  var posTop =(page-1) * $(window).height();
+  mHtml.animate({scrollTop : posTop});
+})
