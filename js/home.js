4c7f3ee1c2433887959b4a7a6ed2ca5163eb5cf0
minseonnn <119590961+minseonnn@users.noreply.github.com>
2023-01-21 22:54:50 +0900

    update

--- js/home.js
+++ js/home.js
@@ -1,23 +1,41 @@
-//스크롤 이벤트 
-window.addEventListener("wheel", function(e){
-  e.preventDefault();
-},{passive : false});
-
-var mHtml = $("html");
-var page = 1;
-
-
-mHtml.animate({scrollTop : 0},10);
-
-$(window).on("wheel", function(e) {
-  if(mHtml.is(":animated")) return;
-  if(e.originalEvent.deltaY > 0) {
-      if(page == 4) return;
-      page++;
-  } else if(e.originalEvent.deltaY < 0) {
-      if(page == 1) return;
-      page--;
+//탑버튼
+$(window).scroll(function(){
+  if($(this).scrollTop() > 0){
+    $('.top_btn').fadeIn();
+  }else {
+    $('.top_btn').fadeOut();
   }
-  var posTop =(page-1) * $(window).height();
-  mHtml.animate({scrollTop : posTop});
-})
+});
+
+
+$('.top_btn').click(function(){
+  $('html,body').animate({scrollTop:0},10);
+});
+
+
+
+//스크롤 이벤트 
+// window.addEventListener("wheel", function(e){
+//   e.preventDefault();
+// },{passive : false});
+
+// var page = 1;
+// var $html = $('html')
+// $html.animate({scrollTop:0},10);
+
+// $(window).on("wheel", function(e) {
+//   if($html.is(":animated")) return;
+//   if(e.originalEvent.deltaY > 0) {
+//     // deltaY값이 양수라면 휠이 아래로 간 것 = 페이지 내림
+//       if(page == 4) return;
+//       page++;
+//   } else if(e.originalEvent.deltaY < 0) {
+//     // deltaY값이 음수라면 휠을 위로 = 페이지 올림
+//       if(page == 1) return;
+//       page--;
+//   }
+
+//   var posTop = $(window).height() * (page-1);
+//   $html.animate({scrollTop : posTop});
+//   //휠을 한번 내릴 때 마다 뷰포트의 크기만큼 움직임
+// })
