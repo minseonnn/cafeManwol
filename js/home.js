ced9c3cec3971f3d2bbe8d13b14f5d5aa01312cb
minseonnn <119590961+minseonnn@users.noreply.github.com>
2023-01-24 00:15:50 +0900

    update2

--- js/home.js
+++ js/home.js
@@ -2,8 +2,19 @@ $(document).ready(function() {
 	$('#fullpage').fullpage({
 		//options here
 		autoScrolling:true,
-		scrollHorizontally: true
+		scrollHorizontally: true,
+    navigation:true,
+    navigationPosition:'right',
+    slidesNavigation:true,
+    controlArrows: false,
+    afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex){
+      if(slideIndex == ) {
+        $('.logo').css('background-image', 'url(../img/logo2.png)');
+      }
+    },
 	});
 
-	
+	document.querySelector('.fp-slidesNav>ul').style.marginLeft='50%';
+
+
 });
