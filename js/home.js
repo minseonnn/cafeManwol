19306ff1261cb4a6df9c4ab1d62318a4c78bf703
minseonnn <119590961+minseonnn@users.noreply.github.com>
2023-01-23 19:27:47 +0900

    update2

--- js/home.js
+++ js/home.js
@@ -1,27 +1,10 @@
+$(document).ready(function() {
+	$('#fullpage').fullpage({
+		//options here
+		autoScrolling:true,
+		scrollHorizontally: true
+	});
 
-
-//스크롤 이벤트 
-window.addEventListener("wheel", function(e){
-  e.preventDefault();
-},{passive : false});
-
-var page = 1;
-var $html = $('html')
-$html.animate({scrollTop:0},10);
-
-$(window).on("wheel", function(e) {
-  if($html.is(":animated")) return;
-  if(e.originalEvent.deltaY > 0) {
-    // deltaY값이 양수라면 휠이 아래로 간 것 = 페이지 내림
-      if(page == 4) return;
-      page++;
-  } else if(e.originalEvent.deltaY < 0) {
-    // deltaY값이 음수라면 휠을 위로 = 페이지 올림
-      if(page == 1) return;
-      page--;
-  }
-
-  var posTop = $(window).height() * (page-1);
-  $html.animate({scrollTop : posTop});
-  //휠을 한번 내릴 때 마다 뷰포트의 크기만큼 움직임
-})
+	//methods
+	$.fn.fullpage.setAllowScrolling(false);
+});
