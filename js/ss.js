0202a8fe2e8133ca9a7d75dc9ff56ca3a27c6eba
minseonnn <119590961+minseonnn@users.noreply.github.com>
2023-01-25 03:39:24 +0900

    update3

--- js/ss.js
+++ js/ss.js
@@ -1,17 +1,17 @@
-let s1 = $('#btn1').offset().top;
-let s2 = $('#btn2').offset().top;
-let s3 = $('#btn3').offset().top;
-let s4 = $('#btn4').offset().top;
-
-$('#btn1').click(function(){
-  $(window).scrollTo(s1);
-});
-$('#btn2').click(function(){
-  $(window).scrollTo(s2);
-});
-$('#btn3').click(function(){
-  $(window).scrollTo(s3);
-});
-$('#btn4').click(function(){
-  $(window).scrollTo(s4);
+var g_interval;
+new fullpage('#fullpage', {
+  licenseKey: 'YOUR KEY HERE',
+  sectionsColor: ['yellow', 'orange', '#C0C0C0', '#ADD8E6'],  
+  afterLoad: function (origin, destination, direction) {
+    clearInterval(g_interval);
+    
+    // 1000 milliseconds lapse
+    const lapse = 1000;
+    
+    if(destination.item.querySelectorAll('.fp-slides').length){
+      g_interval = setInterval(function () {
+        fullpage_api.moveSlideRight();
+      }, lapse);
+    }
+  }
 });
