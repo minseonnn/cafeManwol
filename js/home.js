dc7d22b19358b788f44f012f58cefe1ef0406e06
minseonnn <119590961+minseonnn@users.noreply.github.com>
2023-01-25 04:39:15 +0900

    4:39

--- js/home.js
+++ js/home.js
@@ -24,7 +24,7 @@ $(document).ready(function() {
     }
 	});
 
-	// document.querySelector('.fp-slidesNav>ul').style.marginLeft='50%';
+	document.querySelector('.fp-slidesNav>ul').style.marginLeft='50%';
 
   // let logo = document.querySelector('.logo');
   // let L_index = 0;
