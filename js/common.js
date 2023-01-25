47f4c3d5ff89b3b7f9b38831924a64fea71546e5
minseonnn <119590961+minseonnn@users.noreply.github.com>
2023-01-26 01:52:40 +0900

    ddd

---
+++ js/common.js
@@ -0,0 +1,27 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  // direction: 'vertical',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable : true,
+  },
+
+  // Navigation arrows
+  // navigation: {
+  //   nextEl: '.swiper-button-next',
+  //   prevEl: '.swiper-button-prev',
+  // },
+  speed: 2500,
+  spaceBetween: 20,
+  slidesPerView : 3,
+  // loopAdditionalSlides:1,
+  autoplay: {
+    delay:4500,
+    disableOnInteraction: false,
+  }
+
+});
+
--- js/home.js
+++ js/home.js
@@ -10,22 +10,16 @@ $(document).ready(function() {
     loopBottom: true,
     responsiveWidth: 1200, 
 
-    // 메인에서 상단이동
-		// $('#btnGoTop').click(function () {
-		// 	//$.fn.fullpage.setScrollingSpeed(0); 효과를 없애고싶을때
-		// 	$.fn.fullpage.moveTo(1, 1); // 이동하고싶은 페이지
-		// 	//$.fn.fullpage.setScrollingSpeed(700); 효과를 없애고싶을때
-		// });
+    afterRender: function () {
+      setInterval(function () {
+          $.fn.fullpage.moveSlideRight();
+      }, 4500);
+  }
   
-    // afterRender: function () {
-    //     setInterval(function () {
-    //         $.fn.fullpage.moveSlideRight();
-    //     }, 4500);
-    // }
+
 	});
 
 	document.querySelector('.fp-slidesNav>ul').style.marginLeft='50%';
 
-  
-  
 });
+  
