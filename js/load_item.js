41902821eccc3f5dba85d5ae98abec5437dce5fa
minseonnn <119590961+minseonnn@users.noreply.github.com>
2023-01-29 20:54:28 +0900

    dd

---
+++ js/load_item.js
@@ -0,0 +1,68 @@
+const cate_arr = ['all', 'manwol base', 'manwol goods', 'gift', 'group order']; 
+
+function make_sec(product_id) {  
+  let list = `<div class="product" id="${product_id}">
+                  <div class="pro_body"></div>
+              </div>`;
+  $('.main .pro_container').append(list)
+}
+
+function load_list(cate_idx, start_idx, show_qty) { 
+  $(`#${cate_arr[cate_idx]} > .pro_title`).text(cate_arr[cate_idx].toUpperCase());
+  
+  let rs = ITEM_LIST[cate_idx]; 
+
+
+  let tmp_qty = start_idx+show_qty;
+  if(tmp_qty > rs.length) {
+      tmp_qty = rs.length
+  }
+
+  for(let i=start_idx; i<tmp_qty; i++) {
+      let list = `<div class="item_box">
+                      <div class="item_img">
+                          <a href="deal.html?cate=${cate_idx}&item=${rs[i].item_no}"> 
+                              <img src="${rs[i].src}" alt="">`;
+
+              if(cate_idx == 1)
+                  list += `   <img src="${rs[i].covered_src}" alt=""> `;
+
+                  list += `</a> 
+                      </div>
+                      <div class="item_info">
+                          <div class="item_title">${rs[i].title}</div>
+                          <div class="item_s_price"><del>${rs[i].s_price}</del></div>
+                      </div>
+                  </div>`;
+      $(`#${cate_arr[cate_idx]} > .pro_body`).append(list); 
+
+    
+  }
+}
+
+function get_url_info(key) {
+  let url = location.href; 
+  url = url.split("?"); 
+
+  if(url.length > 1) { 
+      url = url[1];           
+      url = url.split('&');   
+
+      for(let i=0; i<url.length; i++) {
+          let tmp_url = url[i].split("="); 
+         
+
+          if(tmp_url[0] == key) {
+              return tmp_url[1]
+          }
+      }
+      return -1;
+  }
+
+  url = url[1]; 
+  url = url.split("=");
+
+  if(url[0] == key) { 
+
+  }
+} 
---
+++ js/product.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+  let cate_no = get_url_info("cate");
+
+  make_sec(cate_arr[cate_no]);
+  
+  const show_oneTime_qty = 4 * 2;
+  let tmp_one_shot = 0 
+  load_list(cate_no, tmp_one_shot, show_oneTime_qty )
+  tmp_one_shot+=show_oneTime_qty;
+
+
+})
